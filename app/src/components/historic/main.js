ee229f0485c2ee02e69f067e2d66d218bdcb1e03
Sarah Lo <sarahlopezch@icloud.com>
2018-07-28 11:46:20 -0500

    historicoVista

--- app/src/components/historic/main.js
+++ app/src/components/historic/main.js
@@ -2,11 +2,184 @@ import React from 'react';
 import SubHeader from '../general/subheader';
 
 class Historic extends React.Component {
+    constructor(props){
+        super(props);
+        this.toogleList = this.toogleList.bind(this);
+        this.changeSelection = this.changeSelection.bind(this);
+        this.selectRadio = this.selectRadio.bind(this);
+        this.toogleSensor = this.toogleSensor.bind(this);
+        this.askPDF = this.askPDF.bind(this);
+        this.consultar = this.consultar.bind(this);
+        this.state = {
+            selectedMedidor: -1,
+            activeLists: false,
+            types: ["Gráfica", "Tabla"],
+            sensors: [
+                {
+                    name: "VOLTAJE",
+                    active: false
+                },{
+                    name: "CORRIENTE",
+                    active: false
+                },{
+                    name: "P. ACTIVA",
+                    active: false
+                },{
+                    name: "P. REACTIVA",
+                    active: false
+                },{
+                    name: "FDP",
+                    active: false
+                },{
+                    name: "CONSUMO",
+                    active: false
+                },{
+                    name: "SOBRE V.",
+                    active: false
+                }
+                
+            ],
+            typeSelected: 0
+        };
+    }
+    componentDidMount(){
+        this.props.actions.getMedidoresInfo(); //Trae la info de los medidores
+    }
+    askPDF(){
+        /*Función para el boton de pdf*/
+        /*Para mandar los sensores activos mandar this.state.sensors*/
+        console.log("boton de pdf")
+    }
+    consultar(){
+        /*Función para el boton de pdf*/
+        /*Para mandar los sensores activos mandar this.state.sensors*/
+        console.log("boton de consultar")
+    }
+    toogleList(){
+        /*Activa y desactiva la lista de los medidores*/
+        this.setState({
+            activeLists: !this.state.activeLists
+        });
+    }
+    changeSelection(value, name, location){
+        /* Cambia el valor del medidor seleccionado, si se desea hacer otra funcion
+            cada que cambie de medidor se hará en esta funcion antes de cambiar el estado
+        */
+        this.setState({
+            selectedMedidor: value,
+            activeLists: false
+        });
+    }
+    selectRadio(index){
+        /* Cambia el radio button si es gráfica o tabla */
+        this.setState({
+            typeSelected: index 
+        });
+    }
+    toogleSensor(index){
+        /* Recibe el id del boton que hay que activar, es el index del arreglo, y se hace un cambio de true o false */
+        var parametersArray = this.state.sensors;
+        parametersArray[index].active = !parametersArray[index].active;
+
+        this.setState({
+            parametersArray
+        })
+    }
+    renderDropMedidor(){
+        /* Dibuja las opciones de que medidor */
+        var medidorts = this.props.store.medidores;
+        return(
+            <div className="selectionMedidores">
+                <div className={this.state.activeLists === true ? "selectedMedidor activeList" : "selectedMedidor"}>{this.state.selectedMedidor === -1 ? "Selecciona un medidor" : medidorts[this.state.selectedMedidor].labl}</div>
+                <div className="listMeasurer" style={{display: this.state.activeLists === true ? 'block' : 'none'}}>
+                    {medidorts.map((measurer, index2) => (
+                        <div key={index2} className="optionMedidor" onClick={() => this.changeSelection(index2,measurer.name,measurer.ubicacion)}>
+                           <span> {measurer.name}</span>
+                        </div>
+                    ))}
+                </div>
+                <div className="iconContainer"  onClick={this.toogleList}> 
+                    <span className="icon-circle-down"></span>
+                </div>
+            </div>
+        );
+    }
+    renderRadioButtons(){
+        /* Dibuja los radio buttons */
+        return(
+            <div className="radioButtons">
+                {this.state.types.map((opt, index) => (
+                    <div className="buttonRadio" key={index}>
+                        <div className="circleContainer" onClick={()=>this.selectRadio(index)}>
+                            <div className="outsideCircle">
+                                <div className={this.state.typeSelected === index ? "circle active" : "circle"}></div>
+                            </div>
+                        </div>
+                        <span className="name">{opt}</span>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+    renderSensorsActive(){
+        /* Dibuja los de encendido y apagado de cada opcion */
+        return(
+            <div className="sensorButton">
+                {this.state.sensors.map((item, index) => (
+                    <div className="selectionSensor" key={index}>
+                        <div className={item.active === true ? "divCircle active" : "divCircle"} onClick={() => this.toogleSensor(index)}>
+                            <div className={item.active === true ? "circle active" : "circle"}></div>
+                        </div>
+                        <span className="name">{item.name}</span>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+    renderOptions(){
+        return(
+            <div className="optionsDiv">
+                <span className="title"> Periodo de eventos </span>
+                <div className="medidorSelection">
+                    <span className="instruction">Medidor a visualizar</span>
+                    {this.renderDropMedidor()}
+                </div>
+                <div className="selectionOptions">
+                    <div className="periodSelection">
+                        <span className="instruction">Fecha inicial</span>
+                        <input type="date"></input>
+                        <span className="instruction">Fecha final</span>
+                        <input type="date"></input>
+                    </div>
+                    {this.renderRadioButtons()}
+                </div>
+                <div className="sensorsSelection">
+                    <div className="sensorDiv">
+                        <span className="instruction">Sensores: </span>
+                        {this.renderSensorsActive()}
+                    </div>
+                    
+                    <div className="buttonsContainer">
+                        <div className="button pdf" onClick={this.askPDF}>PDF</div>
+                        <div className="button cons" onClick={this.consultar}>Consultar</div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    renderResults(){
+        return(
+            <div>
+                Sección Resultados
+            </div>
+        )
+    }
     render() {
         return (
             <div id="historic">
-            <SubHeader titulo="Histórico de eventos"/>
-            yo soy el historico
+                <SubHeader titulo="Histórico de eventos"/>
+                {this.props.store.medidores != null ? this.renderOptions() : null}
+                {this.renderResults()}
             </div>
         );
     }
--- app/src/flux/store.js
+++ app/src/flux/store.js
@@ -1,178 +1,201 @@
-import actionTypes from './actionTypes'
-import dispatcher from './dispatcher'
-import { EventEmitter } from 'events'
-import assign from 'object-assign'
-// import _ from 'underscore';
-import login from '../data/login.json'
-import menu from '../data/menuTypes.json'
-import medidores from '../data/medidores.json'
-import sensorTypes from '../data/sensorTypes.json'
-import notification from '../data/notificationUsers.json'
-import ranges from '../data/parametersRange.json'
-const CHANGE_EVENT = 'change'
+import actionTypes from './actionTypes';
+import dispatcher from './dispatcher';
+import { EventEmitter } from 'events';
+import assign from 'object-assign';
+//import _ from 'underscore';
+
+const CHANGE_EVENT = 'change';
 
 let AppData = {
-  data: {
-    login: true,
-    userInfo: null,
-    menuOptions: null,
-    sensorTypes: null,
-    notification: null,
-    editUser: {
-      id: '',
-      active: false,
-      name: '',
-      email: ''
+    data:{
+        login: true,
+        userInfo: null,
+        menuOptions: null,
+        sensorTypes: null,
+        notification: null,
+        editUser: {
+            id: "",
+            active: false,
+            name: "",
+            email: ""
+        },
+        myAccount: null,
+        editAccount:{
+            active: false,
+            name: "",
+            email: ""
+        },
+        parameters: null,
+        medidores: null
     },
-    myAccount: null,
-    editAccount: {
-      active: false,
-      name: '',
-      email: ''
+    getUserInfo(){
+        $.getJSON('/app/data/login.js', function(info) {
+            AppData.data.userInfo = info.infoLogin;
+            $.getJSON('/app/data/menuTypes.js', function(info) {
+                AppData.data.menuOptions = info.menuTypes[AppData.data.userInfo.type];
+                AppStore.emitChange();
+            }).fail(function(error) {
+                console.error(error);
+            });
+        }).fail(function(error) {
+            console.error(error);
+        });
     },
-    parameters: null,
-    medidores: null
-  },
-  getUserInfo () {
-    AppData.data.userInfo = login.infoLogin
-    AppData.data.menuOptions = menu.menuTypes[AppData.data.userInfo.type]
-    AppStore.emitChange()
-  },
-  getMedidoresInfo () {
-    AppData.data.medidores = medidores.medidores
-    AppStore.emitChange()
-  },
-  getSensorTypes () {
-    AppData.data.sensorTypes = sensorTypes.sensorTypes
-    AppStore.emitChange()
-  },
-  getNotifications () {
-    AppData.data.notification = notification.NotificationUser // comes from json
-    AppStore.emitChange()
-  },
-  changeValueNotificacion (action) {
-    AppData.data.notification.map((item, index) => {
-      if (action.name === item.name) {
-        AppData.data.notification[index].perday = !action.value
-        AppStore.emitChange()
-      }
-    })
-  },
-  editNotificationData (action) {
-    AppData.data.editUser.id = action.id
-    AppData.data.editUser.active = action.value
-    AppData.data.editUser.name = action.name
-    AppData.data.editUser.email = action.email
-    AppStore.emitChange()
-  },
-  saveChangesEditUser (action) {
-    AppData.data.notification.map((item, index) => {
-      if (action.id === item.id) {
-        AppData.data.notification[index].name = action.name
-        AppData.data.notification[index].email = action.email
-        AppData.data.editUser.active = false
-        AppStore.emitChange()
-      }
-    })
-  },
-  getParametersRange () {
-    // obtiene los valores del back, en este caso tu base de datos con los datos generales
-    // le agregue un valor al json, para que ahora se regrese de esa forma
-    AppData.data.parameters = ranges.parameterRange
-    AppStore.emitChange()
-  },
-  setParametersRange (action) {
-    // Esta funcion se manda a llamar con el Guardar cambios del componente de rango de parametros
-    // en el action.parameters encontraras el nuevo objeto con los nuevos valores
-    console.log(action)
-  },
-  /***********************************************************************************/
-  getMyAccount () {
-    AppData.data.myAccount = login.infoLogin // comes from json
-    AppStore.emitChange()
-  },
-  editMyAccountData (action) {
-    AppData.data.editAccount.active = action.value
-    AppData.data.editAccount.name = action.name
-    AppData.data.editAccount.email = action.email
-    AppStore.emitChange()
-  },
-  saveChangesEditMyAccount (action) {
-    AppData.data.editAccount.active = false
-    AppData.data.myAccount.name = action.name
-    AppData.data.myAccount.email = action.email
-    AppStore.emitChange()
-  }
+    getMedidoresInfo(){
+        $.getJSON('/app/data/medidores.json', function(info) {
+            AppData.data.medidores = info.medidores;
+            AppStore.emitChange();
+        }).fail(function(error) {
+            console.error(error);
+        });
+    },
+    getSensorTypes(){
+        $.getJSON('/app/data/sensorTypes.js', function(info){
+            AppData.data.sensorTypes = info.sensorTypes;
+            AppStore.emitChange();
+        }).fail(function(error) {
+            console.error(error);
+        });
+    },
+    getNotifications(){
+        $.getJSON('/app/data/notificationUsers.js', function(info){
+            AppData.data.notification = info.NotificationUser; // comes from json 
+            AppStore.emitChange();  
+        }).fail(function(error) {
+            console.error(error);
+        });
+    },
+    changeValueNotificacion(action){
+        AppData.data.notification.map((item,index)=>{
+            if(action.name === item.name){
+                AppData.data.notification[index].perday=!action.value;
+                AppStore.emitChange();
+            }
+        });
+    },
+    editNotificationData(action){
+        AppData.data.editUser.id=action.id;
+        AppData.data.editUser.active=action.value;
+        AppData.data.editUser.name=action.name;
+        AppData.data.editUser.email=action.email;
+        AppStore.emitChange();        
+    },
+    saveChangesEditUser(action){
+        AppData.data.notification.map((item,index)=>{
+            if(action.id === item.id){
+                AppData.data.notification[index].name=action.name;
+                AppData.data.notification[index].email=action.email;
+                AppData.data.editUser.active=false;
+                AppStore.emitChange();
+            }
+        });
+    },
+    getParametersRange() {
+        //obtiene los valores del back, en este caso tu base de datos con los datos generales
+        //le agregue un valor al json, para que ahora se regrese de esa forma
+        $.getJSON('/app/data/parametersRange.js', function(info){
+            AppData.data.parameters = info.parameterRange;
+            AppStore.emitChange(); 
+        }).fail(function(error) {
+            console.error(error);
+        });
+    },
+    setParametersRange(action){
+        //Esta funcion se manda a llamar con el Guardar cambios del componente de rango de parametros
+        //en el action.parameters encontraras el nuevo objeto con los nuevos valores
+        console.log(action)
+    },
+/***********************************************************************************/    
+    getMyAccount(){
+        $.getJSON('/app/data/login.js', function(info){
+            AppData.data.myAccount = info.infoLogin; // comes from json 
+            AppStore.emitChange();  
+        }).fail(function(error) {
+            console.error(error);
+        });
+    },
+    editMyAccountData(action){
+        AppData.data.editAccount.active = action.value;
+        AppData.data.editAccount.name=action.name;
+        AppData.data.editAccount.email=action.email;
+        AppStore.emitChange();        
+    },
+    saveChangesEditMyAccount(action){
+        AppData.data.editAccount.active=false;
+        AppData.data.myAccount.name=action.name;
+        AppData.data.myAccount.email=action.email;
+        AppStore.emitChange();
+    }
 }
-/** ************************************************* */
+/*************************************************** */
 
 let AppStore = assign({}, EventEmitter.prototype, {
-  emitChange: function () {
-    this.emit(CHANGE_EVENT)
-  },
-  addChangeListener: function (callback) {
-    this.on(CHANGE_EVENT, callback)
-  },
-  removeChangeListener: function (callback) {
-    this.removeListener(CHANGE_EVENT, callback)
-  }
-})
-/** ************************************************* */
+    emitChange: function() {
+        this.emit(CHANGE_EVENT);
+    },
+    addChangeListener: function(callback) {
+        this.on(CHANGE_EVENT, callback);
+    },
+    removeChangeListener: function (callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    }
+});
+/*************************************************** */
 
 AppStore = assign({}, AppStore, {
-  getData: () => {
-    return AppData.data
-  },
-  getParametersRange: () => {
-    return AppData.data.parameters
-  }
-})
+    getData: () => {
+        return AppData.data;
+    },
+    getParametersRange: () => {
+        return AppData.data.parameters;
+    }
+});
 
 dispatcher.register((action) => {
-  switch (action.type) {
+    switch (action.type) {
     case actionTypes.GET_USERINFO:
-      AppData.getUserInfo()
-      break
+        AppData.getUserInfo();
+        break; 
     case actionTypes.GET_MEDIDORESINFO:
-      AppData.getMedidoresInfo()
-      break
+        AppData.getMedidoresInfo();
+        break; 
     case actionTypes.GET_MENUTYPES:
-      AppData.getMenuTypes()
-      break
+        AppData.getMenuTypes();
+        break;
     case actionTypes.GET_SENSORTYPES:
-      AppData.getSensorTypes()
-      break
+        AppData.getSensorTypes();
+        break;
     case actionTypes.GET_NOTIFICATIONS:
-      AppData.getNotifications()
-      break
+        AppData.getNotifications();
+        break;
     case actionTypes.CHANGE_VALUENOTIFICATION:
-      AppData.changeValueNotificacion(action)
-      break
+        AppData.changeValueNotificacion(action);
+        break;
     case actionTypes.EDIT_NOTIFICATIONDATA:
-      AppData.editNotificationData(action)
-      break
+        AppData.editNotificationData(action);
+        break;
     case actionTypes.SAVE_CHANGESEDITUSER:
-      AppData.saveChangesEditUser(action)
-      break
+        AppData.saveChangesEditUser(action);
+        break;
     case actionTypes.GET_MYACCOUNT:
-      AppData.getMyAccount(action)
-      break
+        AppData.getMyAccount(action);
+        break; 
     case actionTypes.EDIT_MYACCOUNTDATA:
-      AppData.editMyAccountData(action)
-      break
+        AppData.editMyAccountData(action);
+        break; 
     case actionTypes.SAVE_CHANGESEDITMYACCOUNT:
-      AppData.saveChangesEditMyAccount(action)
-      break
+        AppData.saveChangesEditMyAccount(action);
+        break; 
     case actionTypes.GET_PARAMETERSRANGE:
-      AppData.getParametersRange(action)
-      break
+        AppData.getParametersRange(action);
+        break; 
     case actionTypes.SET_PARAMETERSRANGE:
-      AppData.setParametersRange(action)
-      break
+        AppData.setParametersRange(action);
+        break; 
     default:
 
 		// no op
-  }
-})
+    }
+});
 
-module.exports = AppStore
+module.exports = AppStore;
